6ddc3d4d591f699c58e5204f623bb38e214c7f3f
ASmirnov-HORIS <asmirnov@horis.ru>
2021-01-26 13:43:20 +0300

    Update link to PyPI.

--- source/_static/js/custom.js
+++ source/_static/js/custom.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const versionElem = document.querySelector("#navbar .navbar-version b");
   const versionParentElem = versionElem.parentElement;
   const linkElem = document.createElement("a");
-  linkElem.href = "https://pypi.org/project/lets-plot/" + versionElem.textContent;
+  linkElem.href = "https://pypi.org/project/lets-plot";
   linkElem.target = "_blank";
   linkElem.appendChild(versionElem);
   versionParentElem.appendChild(linkElem);
