557ff3af3499292458a7f800ddf5fb2679bd308d
ASmirnov-HORIS <asmirnov@horis.ru>
2021-02-03 21:21:33 +0300

    Update gallery. Now it inherits ggplot2 cheat sheet style of describing functions.

---
+++ docs/_downloads/39975187267e5dd1e60c13ce186b81a8/plot_continuous_scales.py
@@ -0,0 +1,22 @@
+"""
+scale_*_continuous()
+====================
+
+Build a plot with ``scale_*__continuous()`` scale.
+
+"""
+
+import pandas as pd
+
+from lets_plot import *
+LetsPlot.setup_html()
+
+# %%
+
+df = pd.read_csv('https://raw.githubusercontent.com/HIL-HK/lets-plot-examples/master/data/mpg.csv')
+
+# %%
+
+ggplot(df, aes(x='fl')) + \
+    geom_bar(aes(fill='fl')) + \
+    scale_fill_continuous()
