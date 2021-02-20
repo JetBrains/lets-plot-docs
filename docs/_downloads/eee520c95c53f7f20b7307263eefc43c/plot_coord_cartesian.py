0b1065da4cc30e4797d0397efcce2e0bc123d64a
Artem Smirnov <asmirnov@horis.ru>
2021-02-20 12:22:45 +0300

    Updated structure. Fixed errors.

---
+++ docs/_downloads/eee520c95c53f7f20b7307263eefc43c/plot_coord_cartesian.py
@@ -0,0 +1,22 @@
+"""
+coord_cartesian()
+=================
+
+Build a bar plot with ``coord_cartesian()`` coordinate system.
+
+"""
+
+import pandas as pd
+
+from lets_plot import *
+LetsPlot.setup_html()
+
+# %%
+
+df = pd.read_csv('https://raw.githubusercontent.com/HIL-HK/lets-plot-examples/master/data/mpg.csv')
+
+# %%
+
+ggplot(df, aes(x='fl')) + \
+    geom_bar() + \
+    coord_cartesian(ylim=[0, 200])
