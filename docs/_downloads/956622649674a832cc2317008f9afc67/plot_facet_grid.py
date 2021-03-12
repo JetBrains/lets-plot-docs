9437b978466a276c22c879c3a9a2b339bc7b45d6
ASmirnov-HORIS <asmirnov@horis.ru>
2021-03-12 15:25:25 +0300

    Get back docs/ folder.

---
+++ docs/_downloads/956622649674a832cc2317008f9afc67/plot_facet_grid.py
@@ -0,0 +1,36 @@
+"""
+facet_grid()
+============
+
+Build a plot with ``facet_grid()``.
+
+"""
+
+import pandas as pd
+
+from lets_plot import *
+LetsPlot.setup_html()
+
+# %%
+
+df = pd.read_csv('https://raw.githubusercontent.com/HIL-HK/lets-plot-examples/master/data/mpg.csv')
+
+# %%
+
+ggplot(df, aes(x='cty', y='hwy')) + \
+    geom_point() + \
+    facet_grid(x='fl') + \
+    ggsize(800, 300)
+
+# %%
+
+ggplot(df, aes(x='cty', y='hwy')) + \
+    geom_point() + \
+    facet_grid(y='year')
+
+# %%
+
+ggplot(df, aes(x='cty', y='hwy')) + \
+    geom_point() + \
+    facet_grid(x='fl', y='year') + \
+    ggsize(800, 600)
---
+++ docs/_sources/pages/api/lets_plot.scale_shape_identity.rst.txt
@@ -0,0 +1,6 @@
+﻿lets\_plot.scale\_shape\_identity
+=================================
+
+.. currentmodule:: lets_plot
+
+.. autofunction:: scale_shape_identity
