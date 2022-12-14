405bf55d6cccc11d867157504ddf40688c6e4fac
Artem Smirnov <asmirnov@horis.ru>
2022-12-14 15:50:27 +0100

    Add exceptions to the test_source_footprints.

--- test/test_source_footprints.py
+++ test/test_source_footprints.py
@@ -11,6 +11,19 @@ from .generator import generate_pages
 
 BUILD_DIR = "docs"
 
+EXCEPTIONS = {
+    'role': [
+        "lets_plot.bistro.residual.residual_plot.html",
+    ],
+}
+
+def _is_exception(exception, page):
+    for p in EXCEPTIONS[exception]:
+        if p in page:
+            warnings.warn(UserWarning("Could be bad {0} in {1}".format(exception, page)))
+            return True
+    return False
+
 @pytest.mark.parametrize(('page'), generate_pages(BUILD_DIR))
 def test_source_footprints(page):
     with codecs.open(page, 'r', 'utf-8') as f:
@@ -20,7 +33,8 @@ def test_source_footprints(page):
         body = str(soup.body)
         assert not re.search(r'\|([a-z]|-|_)+\|', body) # Bad substitutions
         assert not re.search(r'\. ([a-z]|-|_)+:', body) # Bad directives
-        assert not re.search(r':([a-z]|-|_)+:', body) # Bad roles
+        if not _is_exception('role', page):
+            assert not re.search(r':([a-z]|-|_)+:', body) # Bad roles
         assert not re.search(r'&lt;http', body) # Bad external links
         for ref in soup.select('.std-ref'):
             assert ref.parent.name == 'a' # Bad internal link
