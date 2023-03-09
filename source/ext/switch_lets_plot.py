26824c8f074d843435fe6dc314c0f34a8aa3d10c
Artem Smirnov <asmirnov@horis.ru>
2023-03-09 11:11:26 +0100

    Update extension for switching lets-plot version during the spinx build.

--- source/ext/switch_lets_plot.py
+++ source/ext/switch_lets_plot.py
@@ -8,10 +8,15 @@ import sys
 
 def replace_lib(app, env, docnames):
     if env.config.switch_lets_plot:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "--force-reinstall", "lets_plot", "--user"])
+        version_setup = "" if env.config.lets_plot_version is None else "=={0}".format(env.config.lets_plot_version)
+        subprocess.check_call([
+            sys.executable,
+            "-m", "pip", "install", "--force-reinstall", "lets_plot{0}".format(version_setup), "--user"
+        ])
 
 def setup(app):
     app.add_config_value('switch_lets_plot', False, 'env')
+    app.add_config_value('lets_plot_version', None, 'env')
     app.connect('env-before-read-docs', replace_lib)
     return {
         'version': '0.1',
