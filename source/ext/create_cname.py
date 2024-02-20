610b9b19cfa0f22dfaa7b09a2cd2ae664598a2c0
Artem Smirnov <asmirnov@horis.ru>
2024-02-20 14:49:45 +0100

    Fix extension that creates CNAME file.

--- source/ext/create_cname.py
+++ source/ext/create_cname.py
@@ -3,15 +3,17 @@
 # Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 #
 
-def config_inited_handler(app, config):
-    if not config.cname_url:
+def env_updated_handler(app, env):
+    if not env.config.cname_url:
         return
     with open('{0}/CNAME'.format(app.outdir), 'w') as f:
-        f.write(config.cname_url)
+        f.write(env.config.cname_url)
 
 def setup(app):
     app.add_config_value('cname_url', None, '')
-    app.connect('config-inited', config_inited_handler)
+    # A list of all core events can be found here: https://www.sphinx-doc.org/en/master/extdev/appapi.html#sphinx-core-events
+    # If you start creating a CNAME one step earlier (env-merge-info event), it will be deleted
+    app.connect('env-updated', env_updated_handler)
     return {
-        'version': '0.1',
+        'version': '0.2',
     }
