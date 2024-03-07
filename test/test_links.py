dbeb240c09a2b5434ae44fe0d6ec12640c76603d
Artem Smirnov <asmirnov@horis.ru>
2024-03-07 15:49:08 +0100

    Update tests for links.

--- test/test_links.py
+++ test/test_links.py
@@ -11,7 +11,13 @@ from bs4 import BeautifulSoup
 from .generator import generate_pages
 
 BUILD_DIR = "docs"
+SKIP = ["http://my.tile.com"]
 REPLACE_LP_TO_FORK = True
+REPLACE_TO_BRANCH = "dev"
+REPLACES = {
+    "lets-plot.org": "asmirnov-horis.github.io/lets-plot-docs",
+    "nbviewer.org/github/JetBrains/lets-plot-docs/blob/master/source/examples": "nbviewer.org/github/ASmirnov-HORIS/lets-plot-docs/blob/{0}/source/examples".format(REPLACE_TO_BRANCH),
+}
 
 checked_external_links = set()
 
@@ -53,18 +59,25 @@ def check_page(page, href):
     assert os.path.isfile(os.path.join(os.path.dirname(page), href.split("#")[0]))
 
 def check_external_link(href):
-    SKIP = ["http://my.tile.com"]
-    if REPLACE_LP_TO_FORK:
-        href = href.replace("lets-plot.org", "asmirnov-horis.github.io/lets-plot-docs")
     if href in checked_external_links:
         return
     checked_external_links.add(href)
-    try:
-        response = requests.get(href)
-    except requests.exceptions.ConnectionError:
-        assert href in SKIP
+    response = get_response(href)
+    if response is None:
         return
     assert response.status_code != 404
     if response.status_code == 200:
         return
     warnings.warn(UserWarning("Status code {0} for href={1}".format(response.status_code, href)))
+
+def get_response(href, first_query=True):
+    try:
+        response = requests.get(href)
+        if response.status_code == 404 and REPLACE_LP_TO_FORK and first_query:
+            for replace_from, replace_to in REPLACES.items():
+                href = href.replace(replace_from, replace_to)
+            response = get_response(href, False)
+        return response
+    except requests.exceptions.ConnectionError:
+        assert href in SKIP
+        return None
