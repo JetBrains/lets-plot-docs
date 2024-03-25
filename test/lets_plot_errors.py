fdd92e935831f3d9115b58277b6be3449a5b467d
Artem Smirnov <asmirnov@horis.ru>
2024-03-25 18:10:40 +0100

    Upgrade tests: Find more plot building errors.

--- test/lets_plot_errors.py
+++ test/lets_plot_errors.py
@@ -2,15 +2,20 @@
 
 import warnings
 
+from lxml import etree
 from selenium.webdriver.common.by import By
 
-def check_lets_plot_message_errors(parser, parser_type, source, warn_only=True):
+def check_lets_plot_message_errors(parser, parser_type, source, *, warn_only=False):
     message = "Plot displaying error in {0}".format(source)
     errors_count = 0
     if parser_type == 'driver':
-        errors_count = len(parser.find_elements(By.CSS_SELECTOR, ".lets-plot-message-error"))
+        backend_errors_count = len(parser.find_elements(By.CSS_SELECTOR, ".lets-plot-message-error"))
+        frontend_errors_count = len(parser.find_elements(By.XPATH, "//*[contains(text(), 'Error building plot')]"))
+        errors_count = backend_errors_count + frontend_errors_count
     elif parser_type == 'soup':
-        errors_count = len([s for s in parser.select('script[data-lets-plot-script="plot"]') if "__error_message" in str(s)])
+        backend_errors_count = len([s for s in parser.select('script[data-lets-plot-script="plot"]') if "__error_message" in str(s)])
+        frontend_errors_count = len(etree.HTML(str(parser)).xpath("//*[contains(text(), 'Error building plot')]"))
+        errors_count = backend_errors_count + frontend_errors_count
     else:
         raise ValueError("Bad parser type: {0}".format(parser_type))
     if warn_only:
