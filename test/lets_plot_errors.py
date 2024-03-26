70e4ddc70f3a2a3262d441aedd76406002fd7b85
Artem Smirnov <asmirnov@horis.ru>
2024-03-26 12:21:09 +0100

    Fix test_notebook_has_no_errors.

--- test/lets_plot_errors.py
+++ test/lets_plot_errors.py
@@ -2,22 +2,17 @@
 
 import warnings
 
-from lxml import etree
 from selenium.webdriver.common.by import By
 
 def check_lets_plot_message_errors(parser, parser_type, source, *, warn_only=False):
-    message = "Plot displaying error in {0}".format(source)
-    errors_count = 0
-    if parser_type == 'driver':
-        backend_errors_count = len(parser.find_elements(By.CSS_SELECTOR, ".lets-plot-message-error"))
-        frontend_errors_count = len(parser.find_elements(By.XPATH, "//*[contains(text(), 'Error building plot')]"))
-        errors_count = backend_errors_count + frontend_errors_count
-    elif parser_type == 'soup':
-        backend_errors_count = len([s for s in parser.select('script[data-lets-plot-script="plot"]') if "__error_message" in str(s)])
-        frontend_errors_count = len(etree.HTML(str(parser)).xpath("//*[contains(text(), 'Error building plot')]"))
-        errors_count = backend_errors_count + frontend_errors_count
-    else:
-        raise ValueError("Bad parser type: {0}".format(parser_type))
+    message = ""
+    frontend_errors_count = _get_frontend_errors_count(parser, parser_type)
+    if frontend_errors_count > 0:
+        message = "Plot displaying frontend error in {0}".format(source)
+    backend_errors_count = _get_backend_errors_count(parser, parser_type)
+    if backend_errors_count > 0:
+        message = "Plot displaying backend error in {0}".format(source)
+    errors_count = backend_errors_count + frontend_errors_count
     if warn_only:
         if errors_count > 0:
             warnings.warn(UserWarning(message))
@@ -38,3 +33,22 @@ def check_warnings(parser, parser_type, source):
         raise ValueError("Bad parser type: {0}".format(parser_type))
     for message in warning_messages:
         warnings.warn(UserWarning('Warning in {0} in output cell: "{1}"'.format(source, message)))
+
+def _get_backend_errors_count(parser, parser_type):
+    if parser_type == 'driver':
+        return len(parser.find_elements(By.CSS_SELECTOR, ".lets-plot-message-error"))
+    elif parser_type == 'soup':
+        return len([s for s in parser.select('script[data-lets-plot-script="plot"]') if "__error_message" in str(s)])
+    else:
+        raise ValueError("Bad parser type: {0}".format(parser_type))
+
+def _get_frontend_errors_count(parser, parser_type):
+    if parser_type == 'driver':
+        return sum([
+            1 if "Error building plot" in plot.get_attribute('innerHTML') else 0
+            for plot in parser.find_elements(By.CSS_SELECTOR, ".plt-container")
+        ])
+    elif parser_type == 'soup':
+        return 0 # Pure Beautifulsoup can't extract errors from JS
+    else:
+        raise ValueError("Bad parser type: {0}".format(parser_type))
