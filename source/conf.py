bb379876c9b2464978e629e08e300db92223fb05
Artem Smirnov <asmirnov@horis.ru>
2024-06-26 17:01:36 +0200

    Refactor conf.py (add comments mostly) and add custom 404.

--- source/conf.py
+++ source/conf.py
@@ -22,53 +22,29 @@ sys.path.insert(0, os.path.abspath('./ext'))
 project = 'lets-plot'
 author = 'JetBrains'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'numpydoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.githubpages',
+    # built in extensions
+    "sphinx.ext.autosummary",
+    "sphinx.ext.githubpages",
+    # installed extensions
+    "jupyter_sphinx",
+    "notfound.extension",
+    "numpydoc",
     "sphinxext.opengraph",
     "sphinx_design",
-    'sphinx_reredirects',
-    'jupyter_sphinx',
-    'create_cname',
-    'extref',
-    'switch_lets_plot',
+    "sphinx_reredirects",
+    # custom extensions
+    "create_cname",
+    "extref",
+    "switch_lets_plot",
 ]
 
-cname_url = "lets-plot.org"
-
-extref_conf = "extref_conf.json"
-extref_logo_images = {
-    'colab': "_static/images/logo/colab.svg",
-    'datalore': "_static/images/logo/datalore.svg",
-    'dataspell': "_static/images/logo/dataspell.png",
-    'deepnote': "_static/images/logo/deepnote.svg",
-    'kaggle': "_static/images/logo/kaggle.svg",
-    'nbviewer': "_static/images/logo/jupyter.svg",
-    'nextjournal': "_static/images/logo/nextjournal.svg",
-    'pycharm': "_static/images/logo/pycharm.svg",
-}
-extref_default_image = 'square'
-extref_class = "extref"
-
-autodoc_default_options = {
-    'member-order': 'bysource',
-}
-
-autosummary_generate = True
-
-numpydoc_show_class_members = False
-
-ogp_site_url = "https://lets-plot.org/"
-ogp_site_name = "Lets-Plot"
-ogp_image = "_static/images/open-graph.png"
-
+# -- Sphinx own configuration ------------------------------------------------
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -85,16 +61,64 @@ version = lets_plot.__version__
 # The full version, including alpha/beta/rc tags.
 release = lets_plot.__version__
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
 
-# -- Options for HTML output -------------------------------------------------
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+    'css/colors.css',
+    'css/dataframe.css',
+]
+html_js_files = [
+    'js/custom.js',
+    'js/language_data.js',
+]
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The default sidebars (for documents that don't match any pattern) are
+# defined by theme itself.  Builtin themes are using these templates by
+# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
+# 'searchbox.html']``.
 #
+html_sidebars = {
+    # Default to no sidebar
+    '**': [],
+}
+
+
+# ----------------------------------------------------------------------------
+# -- Built in extensions -----------------------------------------------------
+# ----------------------------------------------------------------------------
+
+# -- Autosummary extension ---------------------------------------------------
+
+autodoc_default_options = {
+    'member-order': 'bysource',
+}
+autosummary_generate = True
+
+
+# ----------------------------------------------------------------------------
+# -- Installed extensions ----------------------------------------------------
+# ----------------------------------------------------------------------------
+
+# -- Built in theme and PyData Sphinx theme extension ------------------------
+
 html_theme = "pydata_sphinx_theme"
 html_show_sourcelink = False
 html_title = ""
-
+html_favicon = "_static/favicon.ico"
+html_context = {
+    'cur_year': datetime.now().year,
+    'python_root_doc': "python/index",
+    'python_root_name': "Lets-Plot for Python",
+}
 html_theme_options = {
     "logo": {
         "alt_text": "Lets-Plot",
@@ -131,46 +155,59 @@ html_theme_options = {
     },
 }
 
-html_favicon = "_static/favicon.ico"
+# -- Not found (404) extension -----------------------------------------------
 
-html_context = {
-    'cur_year': datetime.now().year,
-    'python_root_doc': "python/index",
-    'python_root_name': "Lets-Plot for Python",
+notfound_context = {
+    'title': "Page not found",
+    'body': """<h1>404</h1>
+<h2>Page not found</h2>
+
+<p>
+  Unfortunately we couldn't find the content you were looking for.
+</p>
+<p>
+  <a class="sd-sphinx-override sd-btn sd-text-wrap sd-btn-primary reference internal" href="index.html">Take me back to the homepage</a>
+</p>""",
 }
+notfound_urls_prefix = None
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# -- Numpydoc extension ------------------------------------------------------
 
-# These paths are either relative to html_static_path
-# or fully qualified paths (eg. https://...)
-html_css_files = [
-    'css/custom.css',
-    'css/colors.css',
-    'css/dataframe.css',
-]
-html_js_files = [
-    'js/custom.js',
-    'js/language_data.js',
-]
+numpydoc_show_class_members = False
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# -- OpenGraph extension -----------------------------------------------------
 
-# The default sidebars (for documents that don't match any pattern) are
-# defined by theme itself.  Builtin themes are using these templates by
-# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
-# 'searchbox.html']``.
-#
-html_sidebars = {
-    # Default to no sidebar
-    '**': [],
-}
+ogp_site_url = "https://lets-plot.org/"
+ogp_site_name = "Lets-Plot"
+ogp_image = "_static/images/open-graph.png"
 
-# Redirects
+# -- Sphinx redirects extension ----------------------------------------------
 
 redirects_conf = open("redirects.json")
 redirects = dict(json.load(redirects_conf))
 redirect_html_template_file = "_templates/redirect.html.template"
+
+
+# ----------------------------------------------------------------------------
+# -- Custom extensions -------------------------------------------------------
+# ----------------------------------------------------------------------------
+
+# -- CNAME generator (extension) ---------------------------------------------
+
+cname_url = "lets-plot.org"
+
+# -- Examples references manager (extension) ---------------------------------
+
+extref_conf = "extref_conf.json"
+extref_logo_images = {
+    'colab': "_static/images/logo/colab.svg",
+    'datalore': "_static/images/logo/datalore.svg",
+    'dataspell': "_static/images/logo/dataspell.png",
+    'deepnote': "_static/images/logo/deepnote.svg",
+    'kaggle': "_static/images/logo/kaggle.svg",
+    'nbviewer': "_static/images/logo/jupyter.svg",
+    'nextjournal': "_static/images/logo/nextjournal.svg",
+    'pycharm': "_static/images/logo/pycharm.svg",
+}
+extref_default_image = 'square'
+extref_class = "extref"
