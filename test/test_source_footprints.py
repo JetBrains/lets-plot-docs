84849ed5f6f6ca59d266fc0e1e4fede7dcba35e0
Artem Smirnov <asmirnov@horis.ru>
2024-08-13 17:57:04 +0200

    Clarify warnings for bad roles in the test_source_footprints.py test.

--- test/test_source_footprints.py
+++ test/test_source_footprints.py
@@ -11,20 +11,6 @@ from .generator import generate_pages
 
 BUILD_DIR = "docs"
 
-EXCEPTIONS = {
-    'role': [
-        "lets_plot.bistro.joint.joint_plot.html",
-        "lets_plot.bistro.residual.residual_plot.html",
-    ],
-}
-
-def _is_exception(exception, page):
-    for p in EXCEPTIONS[exception]:
-        if p in page:
-            warnings.warn(UserWarning("Could be bad {0} in {1}".format(exception, page)))
-            return True
-    return False
-
 @pytest.mark.parametrize(('page'), generate_pages(BUILD_DIR))
 def test_source_footprints(page):
     with codecs.open(page, 'r', 'utf-8') as f:
@@ -34,8 +20,9 @@ def test_source_footprints(page):
         body = str(soup.body)
         assert not re.search(r'\|([a-z]|-|_)+\|', body) # Bad substitutions
         assert not re.search(r'\. ([a-z]|-|_)+:', body) # Bad directives
-        if not _is_exception('role', page):
-            assert not re.search(r':([a-z]|-|_)+:', body) # Bad roles
+        roles = re.findall(r'(:([a-z]|-|_)+:)', body) # Bad roles
+        for role, _ in roles:
+            warnings.warn(UserWarning("Could be bad role '{0}' in {1}".format(role, page)))
         assert not re.search(r'&lt;http', body) # Bad external links
         for ref in soup.select('.std-ref'):
             assert ref.parent.name == 'a' # Bad internal link
