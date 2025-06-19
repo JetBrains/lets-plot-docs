70c901b58ea5501cb75740708da506810bf18fdc
Artem Smirnov <asmirnov@horis.ru>
2025-06-19 13:37:07 +0200

    Add test that checks copyright year.

---
+++ test/test_source_files.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+
+import pytest
+
+@pytest.mark.parametrize(('filename'), ["README.md", "LICENSE"])
+def test_copyright_year(filename):
+    expected_current_year = datetime.now().year
+    copyright_prefix = "Copyright © "
+    with open(filename) as f:
+        for l in f:
+            line = l.strip()
+            if not line.startswith(copyright_prefix):
+                continue
+            years_range = line[len(copyright_prefix):len(copyright_prefix)+9]
+            copyright_current_year = int(years_range.split("-")[1])
+            assert expected_current_year == copyright_current_year
+            break
